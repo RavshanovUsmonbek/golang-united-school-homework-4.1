23b70f6439936977e4f5eaabbbbe94089d72bd24
RavshanovUsmonbek <usmonravshanov@list.ru>
2022-05-17 12:05:38 +0500

    initial

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,11 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	tempStr := []rune(input)
+	length := len(tempStr)
+	for i, j := 0, length-1; i < j; i, j = i+1, j+1 {
+		tempStr[i], tempStr[j] = tempStr[j], tempStr[i]
+	}
+	output = string(tempStr)
 	return output
 }
